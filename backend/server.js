ae1578f2adea64621fd1ed153df864d57717022a
Sudheeshmpgt <sudheeshpgt@gmail.com>
2022-05-13 00:42:26 +0530

    port 9000

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const cors=require('cors');
-const PORT = process.env.PORT||9009  
+const PORT = process.env.PORT||9000  
 
 
 app.use(express.json());    
